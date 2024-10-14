2d0ae2e4daf021181a8a2d410e9fc63e14dac3a0
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-13 20:24:24 -0700

    Update Generics.py

--- Generics.py
+++ Generics.py
@@ -1,1 +1,28 @@
+from typing import List
+'''
+class IntList:
+    def __init__(self, elements: List[int]) -> None:
+        self.elements = elements
+
+class FloatList:
+    def __init__(self, elements: List[float]) -> None:
+        self.elements = elements
+
+class StringList:
+    def __init__(self, elements: List[str]) -> None:
+        self.elements = elements
+'''
+class GenericsList[T]:
+    def __init__(self, elements: List[T]) -> None:
+        self.elements = elements
+
+i = GenericsList[int]([1, 2, 3, 4])
+print(sum(i.elements))
+
+f = GenericsList[float]([1.5, 2.5, 2.5, 3.5])
+print(sum(f.elements))
+
+s = GenericsList[str](["1", "2", "3", 4])
+print(s.elements)
+
 
